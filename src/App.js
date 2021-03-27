b2d4da3baf240ea5c0e37802237d68ad97a9971c
unknown <bassem.labbassi@gmail.com>
2021-03-27 16:38:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MovieList from './Components/MovieList';
+
+
+
+function App() {
+  return (
+    <div class="center">
+      <MovieList />
+
+                                                      
+</div>
+  );
+}
+
+export default App;
